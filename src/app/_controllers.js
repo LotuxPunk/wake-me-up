3d82e82b9fc8fd3e74bcbbe059207cb48172d7d3
Paul Leclercq <paul.leclercq@teads.Tv>
2015-03-11 23:13:44 +0100

    remove useless files for gh-pages

--- src/app/_controllers.js
+++
@@ -1,3 +0,0 @@
-angular.module( 'reveilEnLigne.controllers', [
-  'reveilEnLigne.home'
-]);
--- src/app/home/commentsDirective.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('reveilEnLigne.controllers');
-
-app.directive('comments', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'home/comments.tpl.html'
-  };
-});
